b643e3261ef9fbb19178067ded9e56849acf8af7
Sharolinah-23 <kelebogilematjila23@gmail.com>
2024-09-25 15:45:48 +0200

    Added the NavBar,ui folder and editted the footer

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { FooterComponent } from "@/components/footer";
+import FooterComponent from "../components/Footer";
+// import { FooterComponent } from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}<FooterComponent/></body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }

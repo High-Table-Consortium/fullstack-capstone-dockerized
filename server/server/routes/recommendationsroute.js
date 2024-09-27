646c93cd3560164c7f196ecb66719d8c274403f6
Tumelo2748 <tumelothinane13@gmail.com>
2024-09-27 10:09:28 +0200

    Refactor: Add Descriptive Comments to recommendations.js

--- server/server/routes/recommendationsroute.js
+++ server/server/routes/recommendationsroute.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const recommendationsController = require('../controllers/recommendations');
+const {getRecommendations,getRecommendationById,createRecommendation,updateRecommendation,deleteRecommendation} = require('../controllers/recommendations');
 
 // GET all recommendations
-router.get('/', recommendationsController.getRecommendations);
+router.get('/', getRecommendations);
 
 // GET a specific recommendation by ID
-router.get('/:id', recommendationsController.getRecommendationById);
+router.get('/:id', getRecommendationById);
 
 // CREATE a new recommendation
-router.post('/', recommendationsController.createRecommendation);
+router.post('/', createRecommendation);
 
 // UPDATE a recommendation
-router.patch('/:id', recommendationsController.updateRecommendation);
+router.patch('/:id', updateRecommendation);
 
 // DELETE a recommendation
-router.delete('/:id', recommendationsController.deleteRecommendation);
+router.delete('/:id', deleteRecommendation);
 
 module.exports = router;

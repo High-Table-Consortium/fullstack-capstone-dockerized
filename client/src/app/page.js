d857a164a75efc841b3b640bca44a758d238d355
Wesley Mutyambizi <wesleymutyambizi@gmail.com>
2024-10-03 10:48:16 +0200

    Completed pagination file

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -5,6 +5,7 @@ import HomeP from "./homepage/page";
 import SignInPage from "./auth/signin/page";
 import SignUp from "./auth/signup/page";
 import SearchComponent from "@/components/Searchbar";
+import Pagination from "@/components/Pagination";
 
 export default function Home() {
   return (
@@ -15,7 +16,8 @@ export default function Home() {
       {/* <HomeP/> */}
       {/* <SignInPage /> */}
       {/* <SignUp /> */}
-      <SearchComponent />
+      {/* <SearchComponent /> */}
+      <Pagination />
     </main>
   );
 }

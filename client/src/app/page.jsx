2f099bc1c727c46ca6ea4dd19534d7a4236bc7bb
Tumelo2748 <tumelothinane13@gmail.com>
2024-10-03 10:25:14 +0200

    updated the UI for Homepage and fixed some imports on the components

---
+++ client/src/app/page.jsx
@@ -0,0 +1,19 @@
+// import Test from "./Components/Test";
+import Navbar from "../components/Navbar";
+import FooterComponent from "../components/Footer";
+import HomeP from "./homepage/page";
+import SignInPage from "./auth/signin/page";
+import SignUp from "./auth/signup/page";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className=""></div>
+      {/* <Navbar/> */}
+      {/* <FooterComponent /> */}
+      <HomeP/>
+      {/* <SignInPage /> */}
+      {/* <SignUp /> */}
+    </main>
+  );
+}

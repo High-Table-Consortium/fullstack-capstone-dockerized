aad2ddb9afd3bb6fbca85476cb301030302ec3fe
Tumelo2748 <tumelothinane13@gmail.com>
2024-10-03 10:27:41 +0200

    added configuration for images and update components.json

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["encrypted-tbn0.gstatic.com", "www.winetourscapetown.com", "images.squarespace-cdn.com", "silversea-discover.imgix.net", "a.travel-assets.com,", "cdn.britannica.com", "africantravelcanvas.com", "www.tripsavvy.com"],
+  },
+};
 
 export default nextConfig;

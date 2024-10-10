906a42b2dfc905200f94b6342b83cbd2e0864443
Ntokozo Nkosi <nkosintokozot@gmail.com>
2024-10-10 14:00:10 +0200

    added the about us page where users can save and organize their preferred attractions, locations, or activities

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ["encrypted-tbn0.gstatic.com", "www.winetourscapetown.com", "images.squarespace-cdn.com", "silversea-discover.imgix.net", "a.travel-assets.com,", "cdn.britannica.com", "africantravelcanvas.com", "www.tripsavvy.com", "i.ytimg.com"
       ,"www.sanparks.org", "www.isimangaliso.com", "www.drakensberg.org", "www.maropeng.co.za", "www.cango-caves.co.za", "www.hluhluwegamereserve.com", "cdn.getyourguide.com", "www.isibindi.co.za", "www.nondela.com", "sanipasstours.com", "cdn-icons-png.flaticon.com", "thumbs.dreamstime.com"
+    , "images.unsplash.com"
     ],
   },
 };

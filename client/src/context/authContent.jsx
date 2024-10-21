b527f13dfeef4c1a21224eac31d302c9a78e8692
Tumelo2748 <tumelothinane13@gmail.com>
2024-10-21 14:12:04 +0200

    Fixed register functions

--- client/src/context/authContent.jsx
+++ client/src/context/authContent.jsx
@@ -16,10 +16,10 @@ export const AuthProvider = ({ children }) => {
         try {
             const response = await apiGetUserProfile();
             // console.log('API Response:', response);
-            
+
             if (response.success && response.user) {
                 // console.log('Response user data:', response.user);
-                
+
                 if (response.user.email && response.user.firstName && response.user.lastName) {
                     const userData = {
                         id: response.user._id,
@@ -84,15 +84,9 @@ export const AuthProvider = ({ children }) => {
         setError(null);
         try {
             const response = await apiRegister(firstName, lastName, email, password);
-            // console.log('Register API Response:', response);
-            if (response.success) {
-                await fetchUserProfile(); // Fetch user profile after successful registration
-                return true;
-            } else {
-                throw new Error(response.message || "Registration failed.");
-            }
+            console.log(response)
         } catch (error) {
-            const errorMessage = error.message || "Registration failed.";
+            const errorMessage = error.response?.data?.message || error.message || "Registration failed.";
             setError(errorMessage);
             console.error('Registration failed:', errorMessage);
             return false;
@@ -108,7 +102,8 @@ export const AuthProvider = ({ children }) => {
             if (response.success) {
                 setUser(null);
             } else {
-                throw ne            }
+                throw new error
+            }
         } catch (error) {
             console.error('Logout failed:', error);
             setError(error.message || "Logout failed.");

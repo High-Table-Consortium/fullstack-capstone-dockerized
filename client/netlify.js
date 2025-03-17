a551199925c46ebbfeee56d917e47de8211f89c6
Tumelo2748 <tumelothinane13@gmail.com>
2025-03-17 12:13:28 +0200

    configure nextjs for netlify

---
+++ client/netlify.js
@@ -0,0 +1,7 @@
+// This file is used by Netlify to configure the build process
+console.log('Starting Netlify build process...');
+
+// You can add custom build steps here if needed
+// For example, you might want to modify environment variables or perform other setup tasks
+
+console.log('Netlify build process completed successfully!');
